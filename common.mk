33258934c2aa17e4228bc91977cb869ec10584db
Andy Wingo <wingo@pobox.com>
2009-12-15 11:39:19 +0100

    absolute paths in common.mk
    * common.mk: When prepending to the Guile load path, use absolute paths,
      so we have a correct key for the compilation cache.

--- common.mk
+++ common.mk
@@ -6,7 +6,7 @@ gwrapshlibdir = $(pkglibdir)/modules
 SETGWPATH = "(set! %load-path (append (apply append \
                     (map (lambda (l) \
 	                   (let ((ls (symbol->string l))) \
-			      (list (string-append \"$(top_builddir)/\" ls) \
-				    (string-append \"$(top_srcdir)/\" ls))))  \
+			      (list (string-append \"$(abs_top_builddir)/\" ls) \
+				    (string-append \"$(abs_top_srcdir)/\" ls))))  \
 	              '(guile scheme48))) \
-	              '(\"$(top_srcdir)\" \"$(top_srcdir)/lib\") %load-path))"
+	              '(\"$(abs_top_srcdir)\" \"$(abs_top_srcdir)/lib\") %load-path))"
